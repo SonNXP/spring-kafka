149b156a0cd64f1cead9d6e895673ed1346991da
SonNXP <son.nguyen.xp12@gmail.com>
2021-05-18 13:10:57 +0700

    steps to run Spring-Kafka
    Run the app: mvn spring-boot:run
    Run: start-zookeeper.bat
    Run: start-apache-kafka.bat
    Use PostMan to send a req

---
+++ samples/sample-01/start-apache-kafka.bat
@@ -0,0 +1,3 @@
+cd D:\kafka_2.13-2.8.0\bin\windows
+d:
+kafka-server-start.bat ..\..\config\server.properties
---
+++ samples/sample-01/start-zookeeper.bat
@@ -0,0 +1,3 @@
+cd D:\kafka_2.13-2.8.0\bin\windows
+d:
+zookeeper-server-start.bat ..\..\config\zookeeper.properties
